173ab2a3313083e6153747c1d19f504a60b0011d
CodeHabit <codehabbit1@gmail.com>
2025-12-18 12:42:29 +0530

    remove unused model files

--- src/models/brandModel.js
+++
@@ -1,7 +0,0 @@
-import mongoose from "mongoose";
-const brandSchema = new mongoose.Schema({
-  name: { type: String, required: true, trim: true },
-  description: { type: String },
-  isActive: { type: Boolean, default: true },
-}, { timestamps: true });
-export default mongoose.models.brand || mongoose.model("brand", brandSchema);
